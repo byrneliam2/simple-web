1e3276731d09eda83e3261194a4400753a428ec5
Liam <liam.jb1@gmail.com>
2018-04-04 21:00:43 +1200

    Adding more to Python server

--- src/python/com/byrneliam2/simpleweb/pysimpleweb.py
+++ src/python/com/byrneliam2/simpleweb/pysimpleweb.py
@@ -6,25 +6,50 @@ import socket
 import sys
 
 PORT = 8080
+PACKET_SIZE = 1024
 INDEX = "src/web/index.html"
 
 
 def run():
+    servsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        servsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        servsock.bind(('', PORT))
+        servsock.bind((socket.gethostname(), PORT))
+        servsock.listen(1)
         while True:
             sock, addr = servsock.accept()
             process(sock)
             sock.close()
     except socket.error:
-        sys.exit()
+        raise socket.error
+        # sys.exit()
     finally:
         servsock.close()
 
 
-def process():
-    pass
+def process(sock):
+    try:
+        data = get_data(sock)
+        if not data:
+            return
+        request = data.split(' ')[0]
+    except IOError as e:
+        pass
+
+
+def get_data(sock):
+    request = ""
+    data = sock.recv(PACKET_SIZE).decode()
+    while data:
+        request += data
+        data = sock.recv(PACKET_SIZE).decode()
+    return request
+
+
+def response(body):
+    return "HTTP/1.1 200 OK\n" \
+           + "Content-Length: " + body.length() + "\n" \
+           + "Content-Type: text/html; charset=UTF-8\n\n" \
+           + body
 
 
 run()
